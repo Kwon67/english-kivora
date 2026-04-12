65082d370bc0d2be7ff15a7f85604fa614910e4f
Kwon67 <kontakriada0001@gmail.com>
2026-04-12 01:10:23 -0300

    atualização de dados

---
+++ signup-dummy.mjs
@@ -0,0 +1,19 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  'https://odnsaeyrvhbpjtqkvzff.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kbnNhZXlydmhicGp0cWt2emZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzMTU1MjMsImV4cCI6MjA5MDg5MTUyM30.LMPfb2yHkYvVY_JYQ6nd45ANYU9x04km0SfILSAASt4'
+)
+
+async function test() {
+  const { data, error } = await supabase.auth.signUp({
+    email: 'dummyabc123@kivora.com',
+    password: 'bob2626',
+  })
+  if (error) {
+    console.error('Sign Up Error:', error.message)
+    process.exit(1)
+  }
+  console.log('Sign Up Success!')
+}
+test()
