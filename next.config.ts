b8f463f20ce39b213aff43e1c6dd592a1480d9d4
Kwon67 <kontakriada0001@gmail.com>
2026-04-21 14:12:19 -0300

    meljorgag

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,7 @@ const nextConfig: NextConfig = {
   // Optimize images
   images: {
     formats: ['image/avif', 'image/webp'],
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
